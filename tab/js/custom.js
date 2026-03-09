244029c4b906d261ec2c1ce21bc5b588f2cc9b53
arafatarob <mdarafathossenarob@gmail.com>
2026-03-09 11:32:14 +0600

    update

---
+++ tab/js/custom.js
@@ -0,0 +1,28 @@
+const tabButtons = document.querySelectorAll('.tab');
+
+
+for(var i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].addEventListener('click', function() {
+        var tabName = this.dataset.tab;
+        var tabContent = document.getElementById(tabName);
+
+
+        var allTabContent = document.querySelectorAll('.tabs');
+        var allTabButtons = document.querySelectorAll('.tab');
+        for(var j = 0; j < allTabContent.length; j++){
+            allTabContent[j].style.display = 'none';
+        }
+        for(var k = 0; k < allTabButtons.length; k++){
+            allTabButtons[k].classList.remove('active');
+        }
+
+
+        tabContent.style.display = 'block';
+        this.classList.add('active');
+        
+
+        
+    });
+}
+
+document.querySelector('.tab').click();
